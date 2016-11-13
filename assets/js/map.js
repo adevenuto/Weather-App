c72962387d194c2aee7c8d939c7f13ce0ae83ad9
Anthony <anthonydevenuto@gmail.com>
2016-11-13 16:00:19 -0600

    added traffic layer to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -11,8 +11,11 @@ function initMap(lat, lng) {
       {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
       {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
       { "featureType": "all", "elementType": "geometry", "stylers": [ { "color": "#eeeee7" } ] }, { "featureType": "all", "elementType": "labels.text.fill", "stylers": [ { "color": "#4b4b4b" } ] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [ { "color": "#f5f1e6" } ] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [ { "color": "#c9b2a6" } ] }, { "featureType": "administrative.land_parcel", "elementType": "geometry.stroke", "stylers": [ { "color": "#dcd2be" } ] }, { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": [ { "color": "#e3e3d7" } ] }, { "featureType": "landscape.natural", "elementType": "geometry", "stylers": [ { "color": "#e3e3d7" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#e3e3d7" } ] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [ { "color": "#93817c" } ] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [ { "color": "#527042" } ] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [ { "color": "#447530" } ] }, { "featureType": "road", "elementType": "geometry", "stylers": [ { "color": "#f5f1e6" } ] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [ { "color": "#fac613" } ] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#e9bc62" } ] }, { "featureType": "road.highway.controlled_access", "elementType": "geometry", "stylers": [ { "color": "#ff9900" } ] }, { "featureType": "road.highway.controlled_access", "elementType": "geometry.stroke", "stylers": [ { "color": "#db8555" } ] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [ { "color": "#fdfcf8" } ] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [ { "color": "#806b63" } ] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [ { "color": "#e3e3d7" } ] }, { "featureType": "transit.line", "elementType": "labels.text.fill", "stylers": [ { "color": "#8f7d77" } ] }, { "featureType": "transit.line", "elementType": "labels.text.stroke", "stylers": [ { "color": "#eeeee7" } ] }, { "featureType": "transit.station", "elementType": "geometry", "stylers": [ { "color": "#e3e3d7" } ] }, { "featureType": "water", "elementType": "geometry.fill", "stylers": [ { "color": "#1e6aaa" } ] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "color": "#1e6aaa" } ] }
-    ]
+    ],
   });
+var trafficLayer = new google.maps.TrafficLayer();
+trafficLayer.setMap(map);
+
 addMarker(latLng, map);
 }
 
@@ -38,7 +41,7 @@ function addMarker(markerPos, map) {
              weather(markerPos.lat,markerPos.lng);
              // Zoom to marker
              map.panTo(this.getPosition());
-             map.setZoom(10);
+             map.setZoom(8);
           });
       } else {
         window.alert('No results found');
