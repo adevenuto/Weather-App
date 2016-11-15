cee2fb512ca53e883d5165c637131c707aeeba14
Anthony <anthonydevenuto@gmail.com>
2016-11-15 17:36:53 -0600

    debugging Heroku launch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
-
+mongoose.connect('mongodb://admin:1a2s3d4f5g@ds041939.mlab.com:41939/weather_app');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
@@ -15,9 +15,8 @@ app.get('/',function(req, res){
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-mongoose.connect('mongodb://admin:1a2s3d4f5g@ds041939.mlab.com:41939/weather_app', function(err, database) {
+
+app.listen(3000,function(err){
   if (err) return console.log(err);
-    app.listen(3000,function(){
-      console.log('Server Running');
-  });
+  console.log('Server Running');
 });
